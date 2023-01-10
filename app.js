3879f1483fbcfbbbb9092dee83e50b6e5eb8d099
Vaibhav.8june@gmail.com <88285949+colorfulbutterflies@users.noreply.github.com>
2023-01-10 16:55:57 +0530

    Add methods to add and display notes

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 class App{
     constructor(){
-    
+        
+        this.notes=[];
         this.$form=document.querySelector("#form");
         this.$noteTitle=document.querySelector("#note-title");
+        this.$noteText=document.querySelector("#note-text");
+        this.$notes=document.querySelector("#notes");
         this.$formButtons=document.querySelector("#form-buttons");
-        
+        this.$placeholder=document.querySelector("#placeholder");
         // When we start our app we attach all event listeners defined inside addEventListeners method.
         this.addEventListeners();
     }
@@ -17,13 +20,28 @@ class App{
         document.body.addEventListener("click",event=>{
             this.handleFormClick(event);
         })
+
+        // Note that submit is a event in form.
+        this.$form.addEventListener("submit",event=>{
+            // preventDefault will prevent default behavior of HTML forms from happening.
+            event.preventDefault();
+
+            const title=this.$noteTitle.value;
+            const text=this.$noteText.value;
+            const hasNote = title || text;
+            if (hasNote) {
+         
+                this.addNote({ title, text });
+      }
+        })
+
     };
     // Event is passed to callback function. Here mouse click event on body is passed as object to handleFormClick.   
     handleFormClick(event){
         
         // HTMLelement object has a "contains" method which take another HTMLelement object as argument and returns true if the other element is child of first element.
         
-        // event.target is the HTMLelement object which is clicked. If this object is child of the $form HTMLelement the contains method will return true otherwise false.
+        // event.target is the HTMLelement object which is clicked. If this object is child of the $form HTMLelement, the contains method will return true otherwise false.
         const isFromClicked=this.$form.contains(event.target);
 
         if(isFromClicked){
@@ -44,6 +62,43 @@ class App{
         this.$form.classList.remove('form-open');
         this.$noteTitle.style.display="none";
         this.$formButtons.style.display="none";
+        this.$noteTitle.value = '';
+        this.$noteText.value = '';
+    }
+
+
+    addNote(note){
+        const newNote={
+            title:note.title,
+            text:note.text,
+            color:"white",
+            id:this.notes.length > 0 ? this.notes[this.notes.length-1].id+1 : 1,}
+
+            this.notes=[...this.notes,newNote];
+            this.displayNotes();
+            this.closeForm();
+        }
+        // addNote adds note to list object and displaynote displays the objects onto screen.
+    displayNotes(){
+        //show the placeholder only if the number of notes are 0.
+        const hasNotes=this.notes.length>0;
+        this.$placeholder.style.display=hasNotes?"none":"flex";
+
+
+        this.$notes.innerHTML=this.notes.map(note=>`
+            <div style="background:${note.color};"class="note">
+                <div class="${note.title && 'note-title'}">${note.title}</div>
+                <div class="note-text">${note.text}</div>
+                <div class="toolbar-container>
+                <img class="toolbar-color" src="https://icon.now.sh/palette">
+                <img class="toolbar-delete" src="https://icon.now.sh/delete">
+              </div>
+            </div>
+          </div>
+       `).join("");         
+
     }
 }
+
+
 new App()
